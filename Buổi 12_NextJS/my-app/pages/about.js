dbb4605eef5e5a884c630edfb218c030fb55e701
Nguyen Xuan Huy <huynx42@due.edu.vn>
2022-05-22 21:53:30 +0700

    thuchanh nextjs

---
+++ Buổi 12_NextJS/my-app/pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "./components/layouts/index";
+
+export default function About() {
+    return (
+      <Layout>
+          <h1>This is about page</h1>
+      </Layout>
+    )
+  }
